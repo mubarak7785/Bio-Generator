c18333f9692aa39385623520783ee2b0cb5006db
mubarak7785 <mubarakjinabade07@gmail.com>
2022-08-25 12:12:43 +0530

    location school specialization & occupation boxes are created

--- src/components/Biogenerator.jsx
+++ src/components/Biogenerator.jsx
@@ -8,27 +8,43 @@ export const Biogenerator = () => {
           <h2 className="box">Options</h2>
           <div className="box">
             <label>Profile photo</label>
-            <input type="file" ></input>
+            <input type="file"></input>
           </div>
           <div className="box">
             <label>Name</label>
-            <input
-              type="text"
-              
-              placeholder="Enter name"
-             
-            />
+            <input type="text" placeholder="Enter name" />
             <label>Gender</label>
-            <select >
+            <select>
               <option value="male">Male</option>
               <option value="female">Female</option>
             </select>
-            <button >Random name</button>
+            <button>Random name</button>
+          </div>
+          <div className="box">
+            <input type="checkbox" value="false"></input>
+            <label>Location</label>
+            <input type="text"></input>
+            <button>Random Location</button>
+          </div>
+          <div className="box">
+            <input type="checkbox" value="false"></input>
+            <label>School</label>
+            <input type="text"></input>
+            <button>Random School</button>
+            <br />
+            <label>Specialization</label>
+            <input type="text"></input>
+            <button>Random Specialization</button>
+          </div>
+          <div className="box">
+            <input type="checkbox" value="false"></input>
+            <label>Occupation</label>
+            <input type="text"></input>
+            <button>Random Occupation</button>
           </div>
         </div>
         <div className="result" id="result">
           <h2 className="box">Result</h2>
-         
         </div>
       </div>
     </div>
