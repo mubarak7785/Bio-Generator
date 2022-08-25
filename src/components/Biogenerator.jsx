ebb47a7efd1c2e6847c1f90c7aab5f5244229be4
mubarak7785 <mubarakjinabade07@gmail.com>
2022-08-25 12:04:10 +0530

    profile name & gender UI created

--- src/components/Biogenerator.jsx
+++ src/components/Biogenerator.jsx
@@ -1,8 +1,36 @@
-import "./BioGenerator.css"
-export const Biogenerator=()=>{
-    return(
-        <div className="App" id="app">
-               <h1 className="heading">Bio - Generator</h1>
+import "./BioGenerator.css";
+export const Biogenerator = () => {
+  return (
+    <div className="App" id="app">
+      <h1 className="heading">Bio - Generator</h1>
+      <div className="container">
+        <div className="option">
+          <h2 className="box">Options</h2>
+          <div className="box">
+            <label>Profile photo</label>
+            <input type="file" ></input>
+          </div>
+          <div className="box">
+            <label>Name</label>
+            <input
+              type="text"
+              
+              placeholder="Enter name"
+             
+            />
+            <label>Gender</label>
+            <select >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+            <button >Random name</button>
+          </div>
         </div>
-    )
-}
+        <div className="result" id="result">
+          <h2 className="box">Result</h2>
+         
+        </div>
+      </div>
+    </div>
+  );
+};
